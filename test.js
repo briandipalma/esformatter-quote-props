e5b63609c28483d832a4d280f8ee1f8b2bfb3801
Brian Di Palma <offler@gmail.com>
2015-02-02 08:43:20 +0000

    Add a compound data test.

--- test.js
+++ test.js
@@ -25,4 +25,28 @@ mocha.describe('quote props plugin', function() {
 		"}");
 	});
 
+	mocha.it('should remove quotes in compound data', function() {
+		// Given.
+		var codeStr = "var obj = {\
+			'dropDown': 'setField',\
+			'button': 'clickButton',\
+			'field': 'setField',\
+			'fields': 'setField',\
+			'hybridField': 'setField',\
+			'state': 'setState'\
+		}";
+
+		// When.
+		var formattedCode = esformatter.format(codeStr);
+
+		// Then.
+		assert.equal(formattedCode, "var obj = {\n" +
+			"  dropDown: 'setField',\n" +
+			"  button: 'clickButton',\n" +
+			"  field: 'setField',\n" +
+			"  fields: 'setField',\n" +
+			"  hybridField: 'setField',\n" +
+			"  state: 'setState'\n" +
+		"}");
+	});
 });
